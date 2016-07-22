3a92b56b75c1a30d22fe34926026e6bbfe940678
skarimb <skarimbil@walmartlabs.com>
2016-07-22 07:03:11 -0700

    "FINAL COMMIT REALLY"

--- src/main/java/com/example/skarimb/hackathonfirst/myactivity.java
+++ src/main/java/com/example/skarimb/hackathonfirst/myactivity.java
@@ -5,61 +5,160 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class myactivity extends AppCompatActivity {
 
-    private EditText username;
-    private EditText password;
-    private EditText createUsername;
-    private EditText createPassword;
-    private EditText confirmPassword;
-    private EditText email;
-    private EditText warnPopUp;
-    private Spinner emotionSpinner;
+            private EditText username;
+            private EditText password;
+            private EditText createUsername;
+            private EditText createPassword;
+            private EditText confirmPassword;
+            private EditText email;
+            private EditText warnPopUp;
+            private Spinner emotionSpinner;
+            private EditText recipe;
+            private EditText ingredient;
+            private EditText calorie;
+            private EditText prepTime;
+            private EditText difficultyLevel;
+            private EditText servingNum;
+            private List<String> recipeList = new ArrayList<>();
+            private List<String> ingredientList = new ArrayList<>();
+            private List<String> calorieList= new ArrayList<>();
+            private List<String> prepTimeList= new ArrayList<>();
+            private List<String> difficultyLevelList= new ArrayList<>();
+            private List<String> servingNumList= new ArrayList<>();
+            private List<String> moodList= new ArrayList<>();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.log_in);
-        final Button logInButton = (Button) findViewById(R.id.login);
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                username = (EditText) findViewById(R.id.username);
-                password = (EditText) findViewById(R.id.password);
-                setContentView(R.layout.create_account);
-                createUsername = (EditText) findViewById(R.id.createUsername);
-                createPassword = (EditText) findViewById(R.id.createPassword);
-                confirmPassword = (EditText) findViewById(R.id.confirmPassword);
-                email = (EditText) findViewById(R.id.createEmail);
-                createUsername.setText(username.getText());
-                createPassword.setText(password.getText());
+            private int i = 0;
 
-                final Button createAccountButton = (Button) findViewById(R.id.createAccountButton);
-                createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.log_in);
+                moodList.add("Fun Night");
+                moodList.add("Lazy Night");
+                moodList.add("Tired Night");
+                moodList.add("Fun Night with Kids");
+                moodList.add("Fancy Night");
+                moodList.add("Fancy Night");
+                moodList.add("F Night");
+                moodList.add("Fancy Night");
+                moodList.add("Fancy Night");
+
+                recipeList.add("Chicken Enchiladas");
+                recipeList.add("Golden Potato Soup");
+                recipeList.add("Cheesy Chicken Meatballs");
+                recipeList.add("Make Your Own Tacos");
+                recipeList.add("Harvest Cobb Salad");
+                recipeList.add("Coconut-Crusted Tilapia");
+                recipeList.add("Chicken Picatta");
+                recipeList.add("Steamed Mussels in White Wine Broth");
+                recipeList.add("Cuban Pork Sliders");
+
+                ingredientList.add("Medium");
+                ingredientList.add("High");
+                ingredientList.add("Low");
+
+                calorieList.add("Medium");
+                calorieList.add("High");
+                calorieList.add("Low");
+
+                prepTimeList.add("Medium");
+                prepTimeList.add("High");
+                prepTimeList.add("Low");
+
+                difficultyLevelList.add("High");
+                difficultyLevelList.add("Medium");
+                difficultyLevelList.add("Low");
+
+                servingNumList.add("4");
+                servingNumList.add("1");
+                servingNumList.add("2");
+
+                final Button logInButton = (Button) findViewById(R.id.login);
+                logInButton.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
-                        if (createPassword.getText().toString().contentEquals(confirmPassword.getText().toString()))
-                        {
-                            setContentView(R.layout.landing_page);
-                            final Button nextMealButton = (Button) findViewById(R.id.button6);
-                            nextMealButton.setOnClickListener(new View.OnClickListener() {
-                                public void onClick(View v) {
+                        // Perform action on click
+                        username = (EditText) findViewById(R.id.username);
+                        password = (EditText) findViewById(R.id.password);
+                        setContentView(R.layout.create_account);
+                        createUsername = (EditText) findViewById(R.id.createUsername);
+                        createPassword = (EditText) findViewById(R.id.createPassword);
+                        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+                        email = (EditText) findViewById(R.id.createEmail);
+                        createUsername.setText(username.getText());
+                        createPassword.setText(password.getText());
+
+                        final Button createAccountButton = (Button) findViewById(R.id.createAccountButton);
+                        createAccountButton.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v) {
+                                if (createPassword.getText().toString().contentEquals(confirmPassword.getText().toString()))
+                                {
+                                    setContentView(R.layout.list_meals);
+                                    ((Button)findViewById(R.id.nextMealButton)).setOnClickListener(new View.OnClickListener() {
+                                            public void onClick(View v) {
+                                                if (i<2) {
+                                                    emotionSpinner = (Spinner) findViewById(R.id.spinner2);
+                                                    recipe = (EditText) findViewById(R.id.recipe);
+                                                    ingredient = (EditText) findViewById(R.id.ingredient);
+                                                    calorie = (EditText) findViewById(R.id.calorie);
+                                                    prepTime = (EditText) findViewById(R.id.prepTime);
+                                                    difficultyLevel = (EditText) findViewById(R.id.difficultyLevel);
+                                                    servingNum = (EditText) findViewById(R.id.servingNum);
+                                                    System.out.println(String.valueOf(emotionSpinner.getSelectedItem()));
+                                                    if (recipeList.size() >= i) {
+                                                        recipe.setText(recipeList.get(i));
+                                                        ingredient.setText(ingredientList.get(i));
+                                                        calorie.setText(calorieList.get(i));
+                                                        prepTime.setText(prepTimeList.get(i));
+                                                        difficultyLevel.setText(difficultyLevelList.get(i));
+                                                        servingNum.setText(servingNumList.get(i));
+
+                                                        i++;
+                                                    }
+                                                }
+                                                else{
+                                                    setContentView(R.layout.landing_page);
+                                                    final Button salad = (Button) findViewById(R.id.saladprofilebutton);
+                                                    salad.setOnClickListener(new View.OnClickListener() {
+                                                        public void onClick(View v){
+                                                            setContentView(R.layout.salad_profile);
+                                                            final Button Ramsay = (Button) findViewById(R.id.creator);
+                                                            Ramsay.setOnClickListener(new View.OnClickListener() {
+                                                               public void onClick(View v){
+                                                                   setContentView(R.layout.profile);
+                                                               }
+                                                            });
+                                                        }
+                                                    });
+                                                }
+                                            }
+                                        });
+
+//                                    ((Button) findViewById(R.id.button6)).setOnClickListener(new View.OnClickListener() {
+//                                            public void onClick(View v) {
+//                                                setContentView(R.layout.landing_page);
+//                                            }
+//                                        });
+
 
                                 }
-                            });
-                        }
-                        else
-                        {
-                            warnPopUp = (EditText) findViewById(R.id.warnPopUp);
-                            warnPopUp.setVisibility(View.VISIBLE);
-                        }
+                                else
+                                {
+                                    warnPopUp = (EditText) findViewById(R.id.warnPopUp);
+                                    warnPopUp.setVisibility(View.VISIBLE);
+                                }
+                            }
+                        });
                     }
                 });
+
+
             }
-        });
+        }
 
-
-    }
-}
